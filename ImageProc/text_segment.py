5ca717dfb60c0979d01e7d03c15409fcce0f8425
Smriti J <smritijh@buffalo.edu>
2015-11-22 00:46:48 -0500

    updated text_segment with noise removal and optimal kernel

--- ImageProc/text_segment.py
+++ ImageProc/text_segment.py
@@ -12,7 +12,7 @@ import numpy as np
 
 #for filename in os.listdir(""):
 
-image = cv2.imread("/Users/smritijha/amnh/06.jpg")
+image = cv2.imread("/Users/smritijha/amnh/printed_text.png")
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) # grayscale
 img = cv2.medianBlur(gray,5)
 
@@ -22,31 +22,55 @@ img = cv2.medianBlur(gray,5)
 #th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
 
 
-ret,th1 = cv2.threshold(gray,180,255,cv2.THRESH_BINARY)
+ret,th1 = cv2.threshold(gray,170,255,cv2.THRESH_BINARY)
 
+
+
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+
+#kernel
 kernel = np.ones((2,2),np.uint8)
-dilation = cv2.dilate(th1,kernel,iterations = 1)
 
-th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-th3 = cv2.dilate(th3,kernel,iterations = 1)
+#noise removal
+#-------------------
+opening = cv2.morphologyEx(th3,cv2.MORPH_OPEN,kernel, iterations = 2)
+#--------------------
+
+
+#dilate to get connected components
+dilation = cv2.dilate(opening,kernel,iterations = 5)
+
 
 #cv2.imshow('gray_image',gray)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
+'''
+contours,hierarchy = cv2.findContours(dilation,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+print contours
 
-contours,hierarchy = cv2.findContours(th3,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-cnt = contours[0]
-print cnt
-x,y,w,h = cv2.boundingRect(cnt)
-detection_img = cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+for contour in contours:
+    # get rectangle bounding contour
+    [x,y,w,h] = cv2.boundingRect(contour)
 
-cv2.imshow('detection_img',gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # discard areas that are too large
+    if h>300 and w>300:
+        continue
+
+    # discard areas that are too small
+    if h<40 or w<40:
+        continue
+
+    # draw rectangle around contour on original image
+    cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,255),2)
+'''
+
+#cv2.imshow('detection_img',gray)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
 
-titles = ['Original Image', 'Global Thresholding (v = 127)', 'dilation', 'Adaptive Gaussian Thresholding']
-images = [gray, th1, dilation, th3]
+titles = ['Original Image', 'Global Thresholding (v = 180)', 'th3', 'dilated']
+images = [gray, th1, th3, dilation]
 
 for i in xrange(4):
     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
