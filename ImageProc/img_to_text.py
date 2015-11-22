1e7e341bfffc6511c5b813be707db44f2988c70a
Smriti J <smritijh@buffalo.edu>
2015-11-22 12:15:44 -0500

    text-based image segmentation with json metadata

--- ImageProc/img_to_text.py
+++ ImageProc/img_to_text.py
@@ -1,4 +1,5 @@
 """
+smritijha
 Text-based image segmentation
 """
 
@@ -8,8 +9,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 import re
 import json
+import string
 
-rootdir = '/Users/smritijha/AMNH/trial/'
+rootdir = '/Users/smritijha/AMNH/demo/'
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
@@ -50,6 +52,8 @@ for subdir, dirs, files in os.walk(rootdir):
                 #check if we are in the /images directory or not
                 curr_dir = os.path.split(os.path.realpath(subdir))[1]
                 #print curr_dir
+
+                working_dir = str(os.getcwd())
                 
                 #if directory already exists no need to extract images
                 #if any other directiry than images no extraction should be carried out
@@ -65,32 +69,55 @@ for subdir, dirs, files in os.walk(rootdir):
                     year = author_year[1]
 
                     count = 0
+                    data = {}
+                    json_list = []
                     
                     for contour in contours:
-                        # get rectangle bounding contour
-                        [x,y,w,h] = cv2.boundingRect(contour)
+                            # get rectangle bounding contour
+                            [x,y,w,h] = cv2.boundingRect(contour)
     
-                        # discard areas that are too large
-                        if h>30:
-                            continue
+                             # discard areas that are too large
+                            if h>30:
+                                continue
     
-                        # discard areas that are too small
-                        if h<10 or w<20:
-                            continue
+                            # discard areas that are too small
+                            if h<10 or w<20:
+                                continue
     
-                        # draw rectangle around contour on original image
-                        cv2.rectangle(gray,(x,y),(x+w,y+h),(100,0,100),1)
+                             # draw rectangle around contour on original image
+                            cv2.rectangle(gray,(x,y),(x+w,y+h),(100,0,100),1)
 
-                        #save filename as a counter
-                        count = count + 1
-                        filenum = "{0:0>3}".format(count) #format for filename counter 
-                        #print filenum
+                            #save filename as a counter
+                            count = count + 1
+                            filenum = "{0:0>3}".format(count) #format for filename counter 
+                            #print filenum
     
-                        filename = str(filenum)+'.png' 
-                        fullpath = os.path.join(path, filename) 
+                            filename = str(filenum)+'.png' 
+                            fullpath = os.path.join(path, filename) 
 
-                        # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
-                        crop_img = image[y:y+h, x:x+w]
-                        cv2.imwrite(fullpath, crop_img)   
+                            # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
+                            crop_img = image[y:y+h, x:x+w]
+                            cv2.imwrite(fullpath, crop_img)  
+
+                            parent_img = str(subdir)
+                            word_path = str(path)            
+                            
+                            parent_img = string.replace(parent_img, working_dir, '.')
+                            word_path = string.replace(word_path, working_dir, '.')
+
+                            #create a json object for the corresponding 
+                            data['author'] = author
+                            data['year'] = year
+                            data['img_page'] = parent_img
+                            data['word'] = [word_path, "!@#$%",0]
+
+                            json_list.append(data)
+
+                    #dump the list of json objects in 
+                    jsondata = os.path.join(subdir, 'jsondata.txt')
+
+                    with open(jsondata, 'w') as f:
+                        json.dump(json_list, f, ensure_ascii=False)
+
 
 print '-*-end scene-*-'
