e72008a9084bc597e620063a5580d16f68f05cc3
Lise <koprty@gmail.com>
2015-11-22 16:04:08 -0500

    deployment fun stuff :D

--- SUNYdigsUpthePast/app/data.py
+++ SUNYdigsUpthePast/app/data.py
@@ -43,11 +43,7 @@ def addTest(L):
 def clearALL():
     db.word_images.remove()
     db.validated.remove()
-<<<<<<< HEAD
 
-
-=======
->>>>>>> c5b3ad4a5eb89798b2666025a44fbccb9c2ebf4b
 clearALL()
 addTest(d)
 
